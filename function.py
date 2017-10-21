bac76af0c55191d1236bd69bb3e9fc4ca051b709
pranikutty <32954881+pranikutty@users.noreply.github.com>
2017-10-21 15:55:24 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece018
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece018 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+     print "Hello From My Function!"
+
+def my_function_with_args(first,sec):
+    print first + sec
+
+
+    my_function()
+    my_function_with_args("Your name ", "have a great year!")
+
+if __name__ == '__main__':
+    main()
