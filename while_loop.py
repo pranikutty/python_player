d4190140c43e9408b229b0a793650ee97d266c2d
pranikutty <32954881+pranikutty@users.noreply.github.com>
2017-10-21 15:56:29 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece018
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece018 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    count = 0
+    while (count < 9):
+       print 'The count is:', count
+       count = count + 1
+    print "Good bye!"
+
+if __name__ == '__main__':
+    main()
