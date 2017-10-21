440e1d9e5c084f8ccb3be08983e25962f2475aea
pranikutty <32954881+pranikutty@users.noreply.github.com>
2017-10-21 15:16:30 +0530

    Add files via upload

---
+++ about_string.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece018
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece018 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    mystring='hi from guvi'+'test'
+    print(mystring[3:4])
+    print(mystring.split("2"))
+    print(mystring.find("g"))
+
+if __name__ == '__main__':
+    main()
