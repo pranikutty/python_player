08fb802fc114f1ab7b0525cd2381d0bce8e4e544
pranikutty <32954881+pranikutty@users.noreply.github.com>
2017-10-21 15:01:43 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece018
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece018 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    mystring='hi from guvi'+'test'
+    print(mystring)
+
+if __name__ == '__main__':
+    main()
